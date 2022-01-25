0e0e4f18749fa12249b26e5ff2376c7fd499820e
Manish Thakur <manishthakur1297@gmail.com>
2022-01-26 03:06:52 +0530

    If-Else Execise Added

--- execises/variables/variables.go
+++ execises/variables/variables.go
@@ -1,3 +1,12 @@
+//Requirements:
+//* Store your favorite color in a variable using the `var` keyword
+//* Store your birth year and age (in years) in two variables using
+//  compound assignment
+//* Store your first & last initials in two variables using block assignment
+//* Declare (but don't assign!) a variable for your age (in days),
+//  then assign it on the next line by multiplying 365 with the age
+// 	variable created earlier
+
 package main
 
 import "fmt"
